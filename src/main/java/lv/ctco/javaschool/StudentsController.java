80d4f10f965f6ea9aa08277f65053a391af300e6
Camille <makurincamille@inbox.lv>
2016-07-06 14:58:26 +0300

    qwer

--- src/main/java/lv/ctco/javaschool/StudentsController.java
+++ src/main/java/lv/ctco/javaschool/StudentsController.java
@@ -3,9 +3,11 @@ package lv.ctco.javaschool;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/students")
@@ -26,8 +28,15 @@ public class StudentsController {
 
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> studentById(@PathVariable("id") int id) {
-        Student student = students.stream().filter((s) -> s.getId() == id).findFirst().get();
-        return new ResponseEntity<>(student, HttpStatus.OK);
+        try {
+            Student student = students.stream().filter((s) -> s.getId() == id).findFirst().get();
+            return new ResponseEntity<>(student, HttpStatus.OK);
+        }catch(NoSuchElementException e){
+
+            return new ResponseEntity<>("There is no such student.",HttpStatus.NOT_FOUND);
+        }catch (NumberFormatException e){
+            return new ResponseEntity<>("Wrong student id",HttpStatus.BAD_REQUEST);
+        }
     }
 
     @RequestMapping(method = RequestMethod.POST)
