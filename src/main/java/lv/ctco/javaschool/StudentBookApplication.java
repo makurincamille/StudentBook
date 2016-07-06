f9822aa7ff768c1716716a0b797cfdd3ec57f51b
Camille <makurincamille@inbox.lv>
2016-07-06 12:59:32 +0300

    initial commit

---
+++ src/main/java/lv/ctco/javaschool/StudentBookApplication.java
@@ -0,0 +1,12 @@
+package lv.ctco.javaschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentBookApplication.class, args);
+	}
+}
